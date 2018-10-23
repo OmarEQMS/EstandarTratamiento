6f704671cd1cbdd50028aea3115894cc387d23ba
Omar Quintero <omar.quintero.ms@gmail.com>
2018-10-22 20:38:04 -0500

    Amigo Badillo salvo el dia

--- web/js/appEstandar.js
+++ web/js/appEstandar.js
@@ -13,7 +13,7 @@ $(document).ready(function () {
         success: function (response) {
             $("#estandares").html("");
             for(var i = 0; i < response.length; i++){
-                $("#estandares").append("<div class='card mt-3'><div class='btn card-body algoritmosBorde'><div class='row'><div data-id='" + response[i].idNodo + "' class='estandar col-10 col-sm-11'>" + response[i].nombre + "</div><div class='col-2 col-sm-1'><i data-id='" + response[i].idEstandar + "' class='infoEstandar d-block pull-right fas fa-info-circle float-right icono-info' style='font-size:27px' data-placement='top'> </i></div></div></div></div>")
+                $("#estandares").append("<div class='card mt-3'><div class='btn card-body algoritmosBorde'><div class='row'><div data-id='" + response[i].idNodo + "' class='estandar col-10'><span>" + response[i].nombre + "</span></div><div class='col-2 col-sm-1'><i data-id='" + response[i].idEstandar + "' class='infoEstandar d-block pull-right fas fa-info-circle float-right icono-info' style='font-size:27px' data-placement='top'> </i></div></div></div></div>")
             }
             setColors(270,"algoritmosFondo","algoritmosBorde");
         },
